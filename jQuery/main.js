985e361f6895439f283a5464402cd13b0a114c36
82974377 <able028989@gmail.com>
2023-06-02 16:43:19 +0900

    리빙샵아울렛

---
+++ jQuery/main.js
@@ -0,0 +1,41 @@
+$(function(){
+
+    // 메뉴
+    $(".main>li").hover(function(){
+        $(".sub").stop().show();
+    }, function(){
+        $(".sub").stop().hide();
+    })//
+
+    // 이미지슬라이드
+    $(".move li:gt(0)").hide();
+
+    var n= 0; // 0 1 2
+    setInterval(function(){
+
+        $(".move li").eq(n).fadeOut();
+
+        if( n == 2){
+            n=0;
+        }else{
+            n++;
+        }//
+
+        $(".move li").eq(n).fadeIn();
+    }, 3000)//
+
+    // 팝업
+
+    $(".p_click").click(function(){
+        $(".popup").show()
+    })//
+
+
+    $(".close").click(function(){
+        $(".popup").hide()
+    })//
+    
+
+
+
+})//jquery
